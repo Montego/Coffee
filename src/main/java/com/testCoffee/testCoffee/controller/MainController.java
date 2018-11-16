02fe9b18848c3e2db265663607cfc4d8953bd84e
Montego <bar_d91@mail.ru>
2018-11-16 22:39:52 +0300

    multipar file c

--- src/main/java/com/testCoffee/testCoffee/controller/MainController.java
+++ src/main/java/com/testCoffee/testCoffee/controller/MainController.java
@@ -12,15 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @Controller
 public class MainController {
 
     private final CoffeeRepository coffeeRepository;
+
     @Autowired
     public MainController(CoffeeRepository coffeeRepository) {
-        this.coffeeRepository=coffeeRepository;
+        this.coffeeRepository = coffeeRepository;
     }
 
     @GetMapping("/")
@@ -29,30 +28,30 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
-        Iterable<Coffee> coffees=coffeeRepository.findAll();
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Coffee> coffees = coffeeRepository.findAll();
 
-        if(filter!=null&&!filter.isEmpty()){
+        if (filter != null && !filter.isEmpty()) {
             coffees = coffeeRepository.findByName(filter);
 
-        }else{
-            coffees=coffeeRepository.findAll();
+        } else {
+            coffees = coffeeRepository.findAll();
         }
-        model.addAttribute("coffees",coffees);
+        model.addAttribute("coffees", coffees);
         model.addAttribute("filter", filter);
         return "main";
     }
 
     @PostMapping("/main")
     public String add(
-            @RequestParam("file")MultipartFile file,
+            @RequestParam("file") MultipartFile file,
             @AuthenticationPrincipal User user,
             @RequestParam String coffee_name,
-            Model model){
-        Coffee coffee = new Coffee(coffee_name,user);
+            Model model) {
+        Coffee coffee = new Coffee(coffee_name, user);
         coffeeRepository.save(coffee);
-        Iterable<Coffee> coffees=coffeeRepository.findAll();
-        model.addAttribute("coffees",coffees);
+        Iterable<Coffee> coffees = coffeeRepository.findAll();
+        model.addAttribute("coffees", coffees);
         return "main";
     }
 
